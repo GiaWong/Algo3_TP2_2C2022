37ec08b6f33e48ebf0f41fd2f73f60af9493a5d2
Isabella <avaldivia@fi.uba.ar>
2022-11-16 21:03:01 -0300

    DiagramaClase adaptada a los requisitos entrega_2

--- src/main/test/edu/fiuba/algo3/entrega_2/CasoDeUso18/CasoDeUso18Test.java
+++ src/main/test/edu/fiuba/algo3/entrega_2/CasoDeUso18/CasoDeUso18Test.java
@@ -1,12 +1,27 @@
 package edu.fiuba.algo3.entrega_2.CasoDeUso18;
 
+import edu.fiuba.algo3.modelo.Construccion.Acceso;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CasoDeUso18Test {
+
+    //Verificar que todas las unidades cuasen el daño que dicen que causan en sus ataques
 
     // Unidad Zerg ataca a Construccion Protoss
     @Test
     public void UnZerlingAtacaAUnaConstruccionProtossYDeberiaHacerleDanio() {
+        /*
+        Zerling zerling = new Zerling();
+        Acceso acceso = new Acceso();
+        for(int i =0 ; i<=7;i++) {
+            acceso.construir();
+        }
+        zerling.ataqueTierra(acceso);//adentro se pondrá el :  acceso.recibeDanio();
+        assertEquals(499,acceso.obtenerEscudo());// 499 por l rango de ataque que es 1
+    */
+
 
     }
 
