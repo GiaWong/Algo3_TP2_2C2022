c7a344265fd23f415dd4f3138b61bcd0e4d8fcd4
eduardoescuti <eescuti@fi.uba.ar>
2022-11-17 17:08:07 -0300

    Cambio de Modelo.

---
+++ src/main/test/edu/fiuba/algo3/entrega_2/CasoDeUso19/CasoDeUso19.java
@@ -0,0 +1,12 @@
+package edu.fiuba.algo3.entrega_2.CasoDeUso19;
+
+import org.junit.jupiter.api.Test;
+
+public class CasoDeUso19 {
+
+    @Test
+    public void ZerlingAtacaUnScoutYNoDeberiaHacerleDaño(){
+
+    }
+
+}
