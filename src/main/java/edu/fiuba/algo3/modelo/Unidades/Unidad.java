3c1e58f4590c4ff9a4fc12a17715b816ce5b4447
matxcreed <78880776+matxcreed@users.noreply.github.com>
2022-11-28 14:15:26 -0300

    Unidad Nueva exception para comportamiento de zealot

--- src/main/java/edu/fiuba/algo3/modelo/Unidades/Unidad.java
+++ src/main/java/edu/fiuba/algo3/modelo/Unidades/Unidad.java
@@ -2,6 +2,7 @@ package edu.fiuba.algo3.modelo.Unidades;
 
 import edu.fiuba.algo3.modelo.Acciones.*;
 import edu.fiuba.algo3.modelo.Construccion.Construccion;
+import edu.fiuba.algo3.modelo.Exception.UnidadMuerta;
 import edu.fiuba.algo3.modelo.Jugador.Suministro;
 import edu.fiuba.algo3.modelo.Mapa.Coordenada;
 
@@ -61,5 +62,11 @@ public abstract class Unidad {
     public abstract void atacar(Unidad unaUnidad);
 
     public abstract void atacar(Construccion unaConstruccion);
+
+    public void estaOperativo() throws Exception{
+        if(!vida.estaVivo()){
+            throw new UnidadMuerta();
+        }
+    }
 
 }
