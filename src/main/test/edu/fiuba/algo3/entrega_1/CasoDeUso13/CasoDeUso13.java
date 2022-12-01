26dd1d36340cef3be82cc24cbcab5f8b5b999d92
ginooggero <goggero@fi.uba.ar>
2022-12-01 11:40:35 -0300

    Test 13 actualizado

--- src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso13/CasoDeUso13.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso13/CasoDeUso13.java
@@ -2,27 +2,30 @@ package edu.fiuba.algo3.entrega_1.CasoDeUso13;
 
 import edu.fiuba.algo3.modelo.Construccion.Criadero;
 import edu.fiuba.algo3.modelo.Construccion.ReservaProduccion;
+import edu.fiuba.algo3.modelo.Mapa.Coordenada;
 import edu.fiuba.algo3.modelo.Mapa.Mapa;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CasoDeUso13 {
-    /*@Test
+    @Test
     public void SeDestruyeUnCriaderoYLaCasillaQuedaSinConstruccionYSeVuelveConstruirSobreElMohoAnterior(){
+
 
         Mapa mapa = new Mapa();
         Criadero criadero = new Criadero();
         for(int i = 0; i<3; i++){
-            criadero.construir();
+            criadero.avanzarTurno();
         }
-        mapa.casillaConTerrenoMoho(10,10);
-        mapa.agregar(criadero,10,10);
-        mapa.setearRadio();
-        mapa.destruirConstruccion(10,10);
-        mapa.agregar(new ReservaProduccion(),10,11);
-
-
-        assertEquals(true,mapa.hayConstruccion(10,11));
-    }*/
+        Coordenada coordenada = new Coordenada(10,10);
+        mapa.casillaConTerrenoMoho(coordenada);
+        mapa.agregar(criadero,coordenada);
+        mapa.destruirConstruccion(coordenada);
+        mapa.agregar(new ReservaProduccion(),coordenada);
+        assertDoesNotThrow( ()-> {
+            mapa.agregar(new ReservaProduccion(),coordenada);
+        });
+    }
 }
