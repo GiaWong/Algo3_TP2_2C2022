1c9449284bab6fcd19b2a95353adf8ce690e3b9d
Isabella <avaldivia@fi.uba.ar>
2022-12-07 12:50:12 -0300

    avance UI - PantallaMapa con comandos prolijado

--- src/main/java/edu/fiuba/algo3/vista/PantallaMapa.java
+++ src/main/java/edu/fiuba/algo3/vista/PantallaMapa.java
@@ -22,16 +22,14 @@ public class PantallaMapa implements EventHandler<ActionEvent> {
     private Stage stage;
     private int base ;
     private int altura;
-    private GridPane panel;
+    private GridPane panelDelMapaVisual;
 
     public PantallaMapa(Stage stage) {
         this.stage=stage;
-        this.panel = new GridPane();
+        this.panelDelMapaVisual = new GridPane();
         this.base = 20;
         this.altura = 10;
-        mostrarMapa();
-
-
+        //mostrarMapa();
 
     }
 
@@ -43,13 +41,12 @@ public class PantallaMapa implements EventHandler<ActionEvent> {
     private void mostrarMapa() {
 
 
-
         for(int i=0; i< base; i++){
             for(int j=0; j< altura; j++){
 
-                //casillas con botones
+                //cada casilla será un menú
                 MenuButton boton = new MenuButton();
-                boton.setPrefSize(90, 95);
+                boton.setPrefSize(90, 95);//dimensiones de la casilla
                 boton.alignmentProperty();
 
                 MenuItem m1 = new MenuItem("atacar");
@@ -60,39 +57,39 @@ public class PantallaMapa implements EventHandler<ActionEvent> {
 
                 establecerRecursosAlTerreno(boton, i, j, m1,m2);
 
-                boton.setOnAction(e -> realizarAccion(boton)); //para cuando se haga click en los botones
-                this.panel.add(boton, i, j);
+                boton.setOnAction(e -> realizarAccion(boton)); //para cuando se haga click
+                this.panelDelMapaVisual.add(boton, i, j);
             }
         }
 
-        //configurando panel
-        this.panel.setPadding(new Insets(15, 15, 15, 15));
-        this.panel.setAlignment(Pos.CENTER);
-        this.panel.setGridLinesVisible(true);
+        //configurando panel del mapa
+        this.panelDelMapaVisual.setPadding(new Insets(15, 15, 15, 15));
+        this.panelDelMapaVisual.setAlignment(Pos.CENTER);
+        this.panelDelMapaVisual.setGridLinesVisible(true);
         //fondo pantalla
         File fileFondo = new File("imagenes/imagenMapa.png");
         BackgroundImage primerBackGro = new BackgroundImage(new Image(fileFondo.toURI().toString(), 1920, 1080,true,true), BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
 
-
-        HBox cajaH = new HBox();
+        //configurando panel superior para los botones
+        HBox contenedorHorizontal = new HBox();
         Button botonSalir = new Button("Salir del juego");
         Button botonVolverAlMenu = new Button("Volver al menu");
-        cajaH.setAlignment(Pos.TOP_CENTER);
-        cajaH.getChildren().addAll(botonVolverAlMenu, botonSalir);
-        cajaH.setSpacing(40);
-        cajaH.setPadding(new Insets(5));
+        contenedorHorizontal.setAlignment(Pos.TOP_CENTER);
+        contenedorHorizontal.getChildren().addAll(botonVolverAlMenu, botonSalir);
+        contenedorHorizontal.setSpacing(40);
+        contenedorHorizontal.setPadding(new Insets(5));
 
         configurarBotonesSuperiores(botonSalir,botonVolverAlMenu);
 
+        //juntando tode en una sola caja vertical
+        VBox contenedorVertical = new VBox(contenedorHorizontal, panelDelMapaVisual);
+        contenedorVertical.setBackground( new Background(primerBackGro) );
+        contenedorVertical.setAlignment(Pos.CENTER);
+        VBox.setVgrow(contenedorVertical, Priority.ALWAYS );
+        contenedorVertical.setSpacing(30.0d);
 
-        VBox cajaVertical01 = new VBox(cajaH, panel);
-        cajaVertical01.setBackground( new Background(primerBackGro) );
-        cajaVertical01.setAlignment(Pos.CENTER);
-        VBox.setVgrow(cajaVertical01, Priority.ALWAYS );
-        cajaVertical01.setSpacing(30.0d);
-
-
-        Scene inicial = new Scene(cajaVertical01, 1920, 1080);//ancho-largo
+        //nueva escena
+        Scene inicial = new Scene(contenedorVertical, 1920, 1080);//ancho-largo
         this.stage.setScene(inicial);
         this.stage.show();
 
@@ -109,7 +106,7 @@ public class PantallaMapa implements EventHandler<ActionEvent> {
         botonVolverMenu.setOnAction(pantallaAnterior);
     }
 
-
+    //ver why no aparece por consola!!??
     private void realizarAccion(MenuButton boton) {
         //mensaje en la consola
         System.out.print("\nclikeando a la casilla columna: " + GridPane.getColumnIndex(boton) + "  y fila: " + GridPane.getRowIndex(boton));
@@ -130,14 +127,8 @@ public class PantallaMapa implements EventHandler<ActionEvent> {
         if((base == 0 && altura==0) || (base==7 && altura==7) || (base==7 && altura==6) || (base==8 && altura==0) || (base==10 && altura==0) || (base==0 && altura==1)){
 
             File fileFondo = new File("imagenes/volcan.png");
-            BackgroundImage primerBackGro = new BackgroundImage(new Image(fileFondo.toURI().toString(),
-                    30, 35,true,true),
-                    BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT,
-                    BackgroundSize.DEFAULT);
-            boton.setBackground( new Background(primerBackGro) );
-
+            colocarIconoALaCasilla(fileFondo.toURI().toString(),boton);
             configurarOpcionesCasillaConRecurso(boton, m1, m2);
-
         }
 
 
@@ -146,15 +137,19 @@ public class PantallaMapa implements EventHandler<ActionEvent> {
         if((base == 10 && altura==3) || (base==8 && altura==1) || (base==9 && altura==0)|| (base==14 && altura==9) || (base==15 && altura==9) ||(base==16 && altura==9) ){
 
             File fileFondo2 = new File("imagenes/mineral.png");
-            BackgroundImage primerBackGro = new BackgroundImage(new Image(fileFondo2.toURI().toString(),
-                    30, 35,true,true),
-                    BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT,
-                    BackgroundSize.DEFAULT);
-            boton.setBackground( new Background(primerBackGro) );
-
+            colocarIconoALaCasilla(fileFondo2.toURI().toString(),boton);
             configurarOpcionesCasillaConRecurso(boton, m1, m2);
-
         }
+    }
+
+    private void colocarIconoALaCasilla(String unString, MenuButton boton) {
+
+        BackgroundImage primerBackGro = new BackgroundImage(new Image(unString,
+                30, 35,true,true),
+                BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+        boton.setBackground( new Background(primerBackGro) );
+
     }
 
     private void configurarOpcionesCasillaConRecurso(MenuButton boton, MenuItem m1, MenuItem m2) {
