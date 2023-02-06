1f0029157f8665d264c4e90b8ce948af94341ddf
ginooggero <goggero@fi.uba.ar>
2023-02-06 18:47:32 -0300

    Caso 1 actualizado y corriendo

--- src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso1/CasoDeUso1Test.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/CasoDeUso1/CasoDeUso1Test.java
@@ -14,43 +14,105 @@ public class CasoDeUso1Test {
 
     Mapa mapa = mock(Mapa.class);
 
-    @Test
-    public void CriaderoSeIniciaCon3LarvasYUnaEvolucionaAUnZangano() throws EdificioNoEstaOperativo {
-        BancoDeRecursos bancoDeRecursos = new BancoDeRecursos();
-        Criadero criadero = new Criadero(0); //se crea con 3 larvas
-
-        Unidad zangano = criadero.crearZangano(bancoDeRecursos);
-
-        assertNotEquals(null, zangano);
-
-    }
 
     @Test
-    public void EvolucionaUnaLarvaDeCriaderoYAlPasarTurnoDeberiaTener3Larvas() throws EdificioNoEstaOperativo {
-        BancoDeRecursos bancoDeRecursos = new BancoDeRecursos();
-        Criadero criadero = new Criadero(0); //se crea con 3 larvas
-
-        Unidad zangano1 = criadero.crearZangano(bancoDeRecursos);
-        criadero.avanzarTurno(mapa);
-
-        assertNotEquals(null, zangano1);
+    public void CriaderoSeIniciaCon3Larvas() throws EdificioNoEstaOperativo {
+        Criadero criadero = new Criadero(0);
         assertTrue(criadero.tieneMismaCantidadDeLarvas(3));
+    }
+
+    @Test
+    public void EvolucionaUnaLarvaDelCriaderoYLeDebenQuedar2Larvas() throws EdificioNoEstaOperativo {
+        BancoDeRecursos bancoDeRecursos = new BancoDeRecursos();
+        Criadero criadero = new Criadero(0);
+        Unidad zangano1 = criadero.crearZangano(bancoDeRecursos);
+        assertNotNull(zangano1);
+        assertTrue(criadero.tieneMismaCantidadDeLarvas(2));
 
     }
 
     @Test
-    public void EvolucionanDosLarvasDeCriaderoYAlPasarTurnoDeberiaTener2Larvas() throws EdificioNoEstaOperativo {
+    public void EvolucionanDosLarvasDeCriaderoYDebeQuedar1Larva() throws EdificioNoEstaOperativo {
         BancoDeRecursos bancoDeRecursos = new BancoDeRecursos();
         Criadero criadero = new Criadero(0); //se crea con 3 larvas
 
         Unidad zangano1 = criadero.crearZangano(bancoDeRecursos);
         Unidad zangano2 = criadero.crearZangano(bancoDeRecursos);
-        criadero.avanzarTurno(mapa);
 
-        assertNotEquals(null, zangano1);
-        assertNotEquals(null, zangano2);
+        assertNotNull(zangano1);
+        assertNotNull(zangano2);
+        assertTrue(criadero.tieneMismaCantidadDeLarvas(1));
+    }
+
+    @Test
+    public void EvolucionanTresLarvasDeCriaderoYNoDebenQuedarLarvas() throws EdificioNoEstaOperativo {
+        BancoDeRecursos bancoDeRecursos = new BancoDeRecursos();
+        Criadero criadero = new Criadero(0); //se crea con 3 larvas
+
+        Unidad zangano1 = criadero.crearZangano(bancoDeRecursos);
+        Unidad zangano2 = criadero.crearZangano(bancoDeRecursos);
+        Unidad zangano3 = criadero.crearZangano(bancoDeRecursos);
+
+        assertNotNull(zangano1);
+        assertNotNull(zangano2);
+        assertNotNull(zangano3);
+        assertTrue(criadero.tieneMismaCantidadDeLarvas(0));
+    }
+
+
+    @Test
+    public void EvolucionanTresLarvasDeCriaderoYAlPasarUnTurnoDebeQuedar1Larva() throws EdificioNoEstaOperativo {
+        BancoDeRecursos bancoDeRecursos = new BancoDeRecursos();
+        Criadero criadero = new Criadero(0); //se crea con 3 larvas
+
+        Unidad zangano1 = criadero.crearZangano(bancoDeRecursos);
+        Unidad zangano2 = criadero.crearZangano(bancoDeRecursos);
+        Unidad zangano3 = criadero.crearZangano(bancoDeRecursos);
+
+        criadero.avanzarTurno(mapa);
+        assertNotNull(zangano1);
+        assertNotNull(zangano2);
+        assertNotNull(zangano3);
+        assertTrue(criadero.tieneMismaCantidadDeLarvas(1));
+    }
+
+    @Test
+    public void EvolucionanTresLarvasDeCriaderoYAlPasarDosTurnosDebeQuedar2Larvas() throws EdificioNoEstaOperativo {
+        BancoDeRecursos bancoDeRecursos = new BancoDeRecursos();
+        Criadero criadero = new Criadero(0); //se crea con 3 larvas
+
+        Unidad zangano1 = criadero.crearZangano(bancoDeRecursos);
+        Unidad zangano2 = criadero.crearZangano(bancoDeRecursos);
+        Unidad zangano3 = criadero.crearZangano(bancoDeRecursos);
+
+        criadero.avanzarTurno(mapa);
+        criadero.avanzarTurno(mapa);
+        assertNotNull(zangano1);
+        assertNotNull(zangano2);
+        assertNotNull(zangano3);
         assertTrue(criadero.tieneMismaCantidadDeLarvas(2));
     }
+
+    @Test
+    public void EvolucionanTresLarvasDeCriaderoYAlPasar3TurnosDebenQuedar3Larvas() throws EdificioNoEstaOperativo {
+        BancoDeRecursos bancoDeRecursos = new BancoDeRecursos();
+        Criadero criadero = new Criadero(0); //se crea con 3 larvas
+
+        Unidad zangano1 = criadero.crearZangano(bancoDeRecursos);
+        Unidad zangano2 = criadero.crearZangano(bancoDeRecursos);
+        Unidad zangano3 = criadero.crearZangano(bancoDeRecursos);
+
+        criadero.avanzarTurno(mapa);
+        criadero.avanzarTurno(mapa);
+        criadero.avanzarTurno(mapa);
+
+        assertNotNull(zangano1);
+        assertNotNull(zangano2);
+        assertNotNull(zangano3);
+        assertTrue(criadero.tieneMismaCantidadDeLarvas(3));
+    }
+
+
 
 
 }
